1b5627f9f4a906e22991381b56c2ff41fe3e0e22
Dwumrohl <81928766+Dwumrohl@users.noreply.github.com>
2023-03-21 19:09:41 +0400

    Сервис для адресов

---
+++ Data/services/IClientAddressService.cs
@@ -0,0 +1,11 @@
+﻿namespace testBlazor.Data.services
+{
+    public interface IClientAddressService
+    {
+        IEnumerable<ClientAddress> GetClientAddress();
+        void insertClientAddress(ClientAddress address);
+        void deleteClientAddress(long id);
+        void updateClientAddress(long id, ClientAddress address);
+        public ClientAddress getSingleClientAddressById(long id);
+    }
+}
