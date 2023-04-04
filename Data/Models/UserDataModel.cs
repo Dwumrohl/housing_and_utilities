f72f982d291ecf294434c053ec03ca1389fac801
Dwumrohl <81928766+Dwumrohl@users.noreply.github.com>
2023-04-04 12:34:40 +0400

    Фикс формы клиента

---
+++ Data/Models/UserDataModel.cs
@@ -0,0 +1,16 @@
+﻿namespace testBlazor.Data.Models
+{
+    public class UserDataModel
+    {
+        public string? name { get; set; }
+
+        public string? surname { get; set; }
+
+        public string? patronymic { get; set; }
+
+        public string? phoneNumber { get; set; }
+
+        public string? email { get; set; }
+
+    }
+}
